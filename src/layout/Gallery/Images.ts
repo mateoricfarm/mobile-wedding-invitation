2a911874f0dd07d18ed1a50d7766c7464d8bed62
guleegulee <progulee@naver.com>
2025-07-23 11:29:03 +0900

    갤러리 이미지 및 지도, 글씨체 수정

--- src/layout/Gallery/Images.ts
+++ src/layout/Gallery/Images.ts
@@ -1,15 +1,9 @@
-import image01 from '@/assets/images/01.jpg'
+import image01 from '@/assets/images/01 main.jpg'
 import image02 from '@/assets/images/02.jpg'
 import image03 from '@/assets/images/03.jpg'
 import image04 from '@/assets/images/04.jpg'
 import image05 from '@/assets/images/05.jpg'
 import image06 from '@/assets/images/06.jpg'
-import image07 from '@/assets/images/07.jpg'
-import image08 from '@/assets/images/08.jpg'
-import image09 from '@/assets/images/09.jpg'
-import image10 from '@/assets/images/10.jpg'
-import image11 from '@/assets/images/11.jpg'
-import image12 from '@/assets/images/12.jpg'
 
 const images = [   /* 이미지 경로를 리스트로 저장 */
   {
@@ -42,38 +36,9 @@ const images = [   /* 이미지 경로를 리스트로 저장 */
   },{
     alt: 'image06',
     source: image06,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image07',
-    source: image07,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image08',
-    source: image08,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image09',
-    source: image09,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image10',
-    source: image10,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image11',
-    source: image11,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image12',
-    source: image12,
-    width: 640,
-    height: 960
+    width: 960,
+    height: 640
+  
   }
 ];
 
