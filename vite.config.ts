6c711d8e68f0a563adce41f0aaa2f6f0e12e7b33
Kwansik Kim <kwansik@outlook.kr>
2025-09-14 18:30:23 +0900

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
+  // 프로젝트 페이지로 배포 시 레포 이름으로 꼭 맞추기
+  base: '/mobile-wedding-invitation/',
 });
